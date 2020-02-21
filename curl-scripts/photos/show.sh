2cb9998e6f154125acf6e8388e3c003eb59b1473
jingjielim <jingjie3000@gmail.com>
2020-02-21 11:44:44 -0500

    Add TOKEN to index, show curl_scripts

--- curl-scripts/photos/show.sh
+++ curl-scripts/photos/show.sh
@@ -3,7 +3,7 @@
 curl "http://localhost:4741/photos/${ID}" \
   --include \
   --request GET \
-  # --header "Content-Type: application/json" \
-  # --header "Authorization: Token token=${TOKEN}" \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
 
 echo
