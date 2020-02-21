2aeeece8711ffb538b3ebc35d73091896f719095
jingjielim <jingjie3000@gmail.com>
2020-02-21 11:36:55 -0500

    Add relationships, OpenReadController to photos
    Added relationship between photo and user models
    Modified photo and user serializers
    Modified photos controller to use OpenReadController

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class PhotosController < OpenReadController
-  before_action :set_photo, only: %i[show update destroy]
+  before_action :set_photo, only: %i[update destroy]
 
   # GET /photos
   def index
@@ -12,12 +12,12 @@ class PhotosController < OpenReadController
 
   # GET /photos/1
   def show
-    render json: @photo
+    render json: Photo.find(params[:id])
   end
 
   # POST /photos
   def create
-    @photo = Photo.new(photo_params)
+    @photo = current_user.photos.build(photo_params)
 
     if @photo.save
       render json: @photo, status: :created, location: @photo
@@ -38,13 +38,15 @@ class PhotosController < OpenReadController
   # DELETE /photos/1
   def destroy
     @photo.destroy
+
+    head :no_content
   end
 
   private
 
   # Use callbacks to share common setup or constraints between actions.
   def set_photo
-    @photo = Photo.find(params[:id])
+    @photo = current_user.photos.find(params[:id])
   end
 
   # Only allow a trusted parameter "white list" through.
--- app/serializers/photo_serializer.rb
+++ app/serializers/photo_serializer.rb
@@ -1,3 +1,8 @@
 class PhotoSerializer < ActiveModel::Serializer
-  attributes :id, :site, :title, :description
+  attributes :id, :site, :title, :description, :editable
+  belongs_to :user
+
+  def editable
+    scope == object.user
+  end
 end
