b5c97da48b24fe55f27345c2e45757ea94b6a0a7
jingjielim <jingjie3000@gmail.com>
2020-02-24 12:05:06 -0500

    Remove title from photos, add num_comments to photos

--- app/serializers/photo_serializer.rb
+++ app/serializers/photo_serializer.rb
@@ -1,9 +1,13 @@
 class PhotoSerializer < ActiveModel::Serializer
-  attributes :id, :site, :title, :description, :editable, :created_at, :updated_at
+  attributes :id, :site, :description, :editable, :created_at, :updated_at, :num_comments
   belongs_to :user
   has_many :comments
 
   def editable
     scope == object.user
+  end
+
+  def num_comments
+    object.comments.length
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_23_205938) do
+ActiveRecord::Schema.define(version: 2020_02_24_040829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 2020_02_23_205938) do
 
   create_table "photos", force: :cascade do |t|
     t.text "site"
-    t.text "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
